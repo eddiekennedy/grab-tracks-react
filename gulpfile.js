fcf78d13f6eded3b814ef3b338a9d0b810c57301
Eddie Kennedy <eddiekennedy@gmail.com>
2016-02-16 23:28:28 -0500

    basic gulp setup

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+// Gulp
+var gulp = require('gulp');
+
+// Plugins
+var server = require('gulp-server-livereload');
+
+// Server
+gulp.task('server', function() {
+  gulp.src('./')
+    .pipe(server({
+      livereload: true,
+      open: true
+    }));
+});
+
+// Watch
+gulp.task('watch', function () {
+  gulp.watch(
+    ['./scripts/*js']
+  );
+});
+
+// Default Task
+gulp.task('default', ['server', 'watch']);
--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,21 @@
+var app = {};
+app.apiRoot = 'https://api.soundcloud.com';
+app.clientId = 'f652822b93b6a6799336b4a729d50de8';
+
+var GrabTracks = React.createClass({
+
+	render: function() {
+		return (
+			<div className="grab-tracks">
+				<h1>Grab Tracks</h1>
+			</div>
+		)
+	}
+
+});
+
+ReactDOM.render(
+  <GrabTracks />,
+  document.getElementById('app')
+);
 
